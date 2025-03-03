da1a89dbbe2d05635994555acf1299175c85bb05
Marko <e3plfmen@ENANW2SD02NB100>
2025-03-03 17:04:44 -0500

    Add project files.

---
+++ Chapter02/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+//using System.Diagnostics;
+//using Chapter02.Models;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace Chapter02.Controllers
+//{
+//    public class HomeController : Controller
+//    {
+//        private readonly ILogger<HomeController> _logger;
+
+//        public HomeController(ILogger<HomeController> logger)
+//        {
+//            _logger = logger;
+//        }
+
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+
+//        public IActionResult Privacy()
+//        {
+//            return View();
+//        }
+
+//        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//        public IActionResult Error()
+//        {
+//            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//        }
+//    }
+//}
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            //return View("MyView");
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView", viewModel);
+        }
+    }
+}
